cfc573cc1d7fded3215f1b9146d7c6890e6692ca
Richard Shi <richard.540102@gmail.com>
2022-10-31 11:31:59 -0400

    Create Tag entity and unit tests.

---
+++ src/main/java/entities/Tag.java
@@ -0,0 +1,37 @@
+package entities;
+
+import java.util.ArrayList;
+
+public class Tag {
+    // The name of the tag
+    final String name;
+
+    // Array of posts that are under this tag
+    final ArrayList<Post> taggedPosts;
+
+    /**
+     * Initializes an instance of Tag.
+     *
+     * @param name The name of this tag.
+     */
+    public Tag(String name) {
+        this.name = name;
+        this.taggedPosts = new ArrayList<>();
+    }
+
+    /**
+     * Add a post that has this tag.
+     *
+     * @param post The post that has this name as a tag.
+     */
+    public void addTag(Post post) {
+        taggedPosts.add(post);
+    }
+
+    /**
+     * Returns the name of this tag.
+     */
+    public String getName() {
+        return this.name;
+    }
+}
