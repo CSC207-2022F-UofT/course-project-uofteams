a9da22389183e52316b662851f911a8bc4121907
reganq <regan.quartus@mail.utoronto.ca>
2022-11-01 01:10:28 -0400

    Added comments to User file. Overrode equals method for user. Added test suite which will be added to once posted.

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,33 +1,61 @@
 package entities;
 
 public class User {
+    // ArrayList of User's favourited posts
     private ArrayList<Post> favourites;
-    private ArrayList<Post> posts;
-    private boolean isAdmin;
-    private String userName;
-    private String password;
 
-    public User(String name, String pass, boolean admin) {
-        this.favourites = new ArrayList();
-        this.posts = new ArrayList();
+    // ArrayList of Posts User has made
+    private ArrayList<Post> posts;
+
+    // boolean relating if User has admin privileges
+    private boolean isAdmin;
+
+    // Number of Users created
+    public static int numUsers = 0;
+
+    // id is the unique identifier of User, equal to number of posts at time of creation
+    private int id;
+
+    /*
+    * Initializes an instance of User
+    *
+    * @param admin whether the user will have admin priviledges
+    * */
+    public User(boolean admin) {
+        this.favourites = new ArrayList<Post>();
+        this.posts = new ArrayList<Post>();
         this.isAdmin = admin;
-        this.userName = name;
-        this.password = pass;
+        User.numUsers ++;
+        this.id = User.numUsers;
     }
 
+    /*
+    * Returns User's favourites
+    *
+    * @return User's favourites attribute
+    * */
     public ArrayList getFavourites() {
         /* Return an ArrayList containing the favourites of this User */
         return this.favourites;
     }
 
+    /*
+    * Adds a Post to User's favourites
+    *
+    * @param toAdd The Post to add to User's favourites
+    * */
     public void addFavourite(Post toAdd) {
         /* Add Post to this.favourites */
-        this.favourites.add(Post);
+        this.favourites.add(toAdd);
     }
 
+    /*
+    * Check if a Post is in User's favourites and remove it if so
+    *
+    * @param toRemove Post to be removed
+    * @return boolean representing success or failure
+    * */
     public boolean removeFavourite(Post toRemove) {
-        /* Search for Post in this.favourites, remove the first instance found.
-         * Return true if found and removed and return false otherwise */
         for (int i = 0; i < this.favourites.size(); i++) {
             if (this.favourites.get(i).equals(toRemove)) {
                 this.favourites.remove(i);
@@ -37,24 +65,32 @@ public class User {
         return false;
     }
 
-    public boolean checkPassword(String toCheck) {
-        if this.password.equals(toCheck) {
-            return true;
-        } else{
-            return false;
-        }
-    }
-
+    /*
+    * Return Posts made by User
+    *
+    * @return User's posts attribute
+    * */
     public ArrayList getPosts() {
         /* return an ArrayList containing the elements of this.posts */
         return this.posts;
     }
 
+    /*
+    * Add a Post to User's posts
+    *
+    * @param toAdd post to be added
+    * */
     public void addPost(Post toAdd) {
         /* add toAdd to this.posts */
         this.posts.add(toAdd);
     }
 
+    /*
+    * Check if a Post is in User's posts and remove it if so
+    *
+    * @param toRemove post to be removed
+    * @returns a boolean representing success or failure
+    * */
     public boolean removePost(Post toRemove) {
         /* Search for toRemove in this.posts and if it is the remove and return true,
          * otherwise return false */
@@ -67,7 +103,31 @@ public class User {
         return false;
     }
 
+    /*
+    * Checks whether User has admin privledges
+    *
+    * @return a boolean which represents whether User has admin priviledges
+    * */
     public boolean isAdmin(){
         return this.isAdmin;
+    }
+
+    /*
+    * Return if User is equal to o
+    *
+    * @param o The Object to be compared to
+    * @return boolean representing whether User is equal to o
+    * */
+    @Override
+    public boolean equals (Object o) {
+        if (this == o) {
+            return true;
+        } else if (o == null) {
+            return false;
+        } else if (!(o instanceof User)) {
+            return false;
+        }  else {
+            return (this.id == ((User) o).id);
+        }
     }
 }
