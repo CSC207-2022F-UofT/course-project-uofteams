917bbeb52341bb1fac1dd549c21ebb3ffcbc7587
izumilocal <izumi.ando@mail.utoronto.ca>
2022-11-03 16:19:23 -0400

    Added getters for body and user instance attributes

--- src/main/java/entities/Comment.java
+++ src/main/java/entities/Comment.java
@@ -2,9 +2,17 @@ package entities;
 
 public class Comment extends Postable{
     // Class Comment extends abstract class Postable and inherits all of its instance
-    // (user, body, replies) and addComment method (which has already been implemented)
+    // attributes (user, body, replies) and addComment method
+    // (which has already been implemented)
     public Comment(User commenter, String comment){
         super.user = commenter;
         super.body = comment;
+    }
+    public String getBody(){
+        return super.body;
+    }
+
+    public User getUser(){
+        return super.user;
     }
 }
