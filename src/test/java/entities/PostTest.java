6ecd359af55d8a216a60546fbb25ce92edce370d
JohannLeidenfrost <60936318+JohannLeidenfrost@users.noreply.github.com>
2022-11-06 17:02:00 -0500

    Added and completed test cases.

--- src/test/java/entities/PostTest.java
+++ src/test/java/entities/PostTest.java
@@ -14,7 +14,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PostTest extends Postable {
     User poster = new User();
-    Timer timer = new Timer();
     String title = "Title";
     String mainDesc = "Main Description";
     String collaborators = "I would like to play tennis doubles with some fellow beginners"
@@ -26,6 +25,7 @@ class PostTest extends Postable {
 
     @BeforeEach
     void setUp() {
+
     }
 
     @AfterEach
@@ -34,38 +34,124 @@ class PostTest extends Postable {
 
     @Test
     void getFavouritedUsers() {
+        calendar.set(2022, 11, 31, 23, 59, 59);
+        Date deadline = calendar.getTime();
+        Post newPost = new Post(poster, title, mainDesc, tags, collaborators, deadline, 0);
+        User user = new User(false, 0, "test-email", "test-password");
+        newPost.addFavouritedUser(user);
+        assertEquals(user, newPost.getFavouritedUsers().get(0));
     }
 
     @Test
     void getNumFavouritedUsers() {
+        calendar.set(2022, 11, 31, 23, 59, 59);
+        Date deadline = calendar.getTime();
+        Post newPost = new Post(poster, title, mainDesc, tags, collaborators, deadline, 0);
+        User user = new User(false, 0, "test-email", "test-password");
+        newPost.addFavouritedUser(user);
+        assertEquals(1, newPost.getNumFavouritedUsers());
     }
 
     @Test
     void addFavouritedUser() {
+        calendar.set(2022, 11, 31, 23, 59, 59);
+        Date deadline = calendar.getTime();
+        Post newPost = new Post(poster, title, mainDesc, tags, collaborators, deadline, 0);
+        User user = new User(false, 0, "test-email", "test-password");
+        User user2 = new User(false, 1, "test-email", "test-password");
+        newPost.addFavouritedUser(user);
+        newPost.addFavouritedUser(user);
+        assertEquals(user, newPost.getFavouritedUsers().get(0));
+        assertEquals(1, newPost.getFavouritedUsers().size());
     }
 
     @Test
     void removeFavouritedUser() {
+        calendar.set(2022, 11, 31, 23, 59, 59);
+        Date deadline = calendar.getTime();
+        Post newPost = new Post(poster, title, mainDesc, tags, collaborators, deadline, 0);
+        User user = new User(false, 0, "test-email", "test-password");
+        User user2 = new User(false, 1, "test-email", "test-password");
+        newPost.addFavouritedUser(user);
+        newPost.removeFavouritedUser(user);
+        assertEquals(0, newPost.getFavouritedUsers().size());
     }
 
     @Test
     void setTags() {
         calendar.set(2022, 11, 31, 23, 59, 59);
         Date deadline = calendar.getTime();
-        Post newPost = new Post(poster, timer, title, mainDesc, tags, collaborators, deadline, replies, 0);
+        Post newPost = new Post(poster, title, mainDesc, tags, collaborators, deadline, 0);
         newPost.setTags(new ArrayList(Arrays.asList("Sports", "Club", "Tennis", "Beginners")));
-        List<String> expected = new ArrayList<>(Arrays.asList("Sports", "Club", "Tennis", "Beginners"));
         assertTrue(newPost.getTags().contains("Beginners"));
+        newPost.setTags(new ArrayList(Arrays.asList()));
+        assertTrue(newPost.getTags().size() == 0);
     }
 
     @Test
     void testEquals() {
         calendar.set(2022, 11, 31, 23, 59, 59);
         Date deadline = calendar.getTime();
-        Post newPost = new Post(poster, timer, title, mainDesc, tags, collaborators, deadline, replies, 0);
+        Post newPost = new Post(poster, title, mainDesc, tags, collaborators, deadline, 0);
         Post aliasPost = newPost;
-        Post newPost2 = new Post(poster, timer, title, mainDesc, tags, collaborators, deadline, replies, 0);
+        Post newPost2 = new Post(poster, title, mainDesc, tags, collaborators, deadline, 1);
         assertFalse(newPost.equals(newPost2));
         assertTrue(newPost.equals(aliasPost));
+    }
+
+    @Test
+    void getID() {
+        calendar.set(2022, 11, 31, 23, 59, 59);
+        Date deadline = calendar.getTime();
+        Post newPost = new Post(poster, title, mainDesc, tags, collaborators, deadline, 0);
+        assertEquals(1, newPost.getID());
+    }
+
+    @Test
+    void getTags() {
+        calendar.set(2022, 11, 31, 23, 59, 59);
+        Date deadline = calendar.getTime();
+        Post newPost = new Post(poster, title, mainDesc, tags, collaborators, deadline, 0);
+        newPost.setTags(new ArrayList(Arrays.asList("Sports")));
+        assertEquals("Sports", newPost.getTags().get(0));
+    }
+
+    @Test
+    void getDeadline() {
+        calendar.set(2022, 11, 31, 23, 59, 59);
+        Date deadline = calendar.getTime();
+        Post newPost = new Post(poster, title, mainDesc, tags, collaborators, deadline, 0);
+        assertEquals(deadline, newPost.getDeadline());
+    }
+    @Test
+    void getReply(){
+        calendar.set(2022, 11, 31, 23, 59, 59);
+        Date deadline = calendar.getTime();
+        Post newPost = new Post(poster, title, mainDesc, tags, collaborators, deadline, 0);
+        Comment comment = new Comment(poster, "Hello!", 1);
+        newPost.addReply(comment);
+        assertEquals(comment, newPost.getReplies().get(0));
+    }
+    @Test
+    void addReply() {
+        calendar.set(2022, 11, 31, 23, 59, 59);
+        Date deadline = calendar.getTime();
+        Post newPost = new Post(poster, title, mainDesc, tags, collaborators, deadline, 0);
+        Comment comment = new Comment(poster, "Hello!", 1);
+        newPost.addReply(comment);
+        newPost.addReply(comment);
+        assertEquals(1, newPost.getReplies().size());
+        assertEquals(comment, newPost.getReplies().get(0));
+    }
+
+    @Test
+    void removeReply() {
+        calendar.set(2022, 11, 31, 23, 59, 59);
+        Date deadline = calendar.getTime();
+        Post newPost = new Post(poster, title, mainDesc, tags, collaborators, deadline, 0);
+        Comment comment = new Comment(poster, "Hello!", 1);
+        newPost.addReply(comment);
+        newPost.removeReply(comment);
+        assertEquals(0, newPost.getReplies().size());
     }
 }
