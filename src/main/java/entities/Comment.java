c88be57818edbeebcf4e04c64a38de129059d149
izumilocal <izumi.ando@mail.utoronto.ca>
2022-11-09 12:55:21 -0500

    - fixed append to add in the addComment method in Postable - added getters getReplies and getCreationDate in Postable - added id & numComments attribute in Comment to keep track of the number of comments instantiated in the program - Overrode the equals method in Comments

--- src/main/java/entities/Comment.java
+++ src/main/java/entities/Comment.java
@@ -7,30 +7,62 @@ import java.util.List;
 
 import java.security.PublicKey;
 
-
+/**
+ * Comment extends abstract class Postable and inherits all of its
+ * instance attributes (user, body, replies) and addComment method.
+ *
+ */
 public class Comment extends Postable{
-    // Class Comment extends abstract class Postable and inherits all of its instance
-    // attributes (user, body, replies) and addComment method
-    // (which has already been implemented)
-    public static Date creationDate;
+    // number of Comments instantiated
+    public static int numComments = 0;
+    // id is the unique identifier of Comment, equal to number of Comments at time of instantiation
+    private final int id;
 
-    public Comment(User commenter, String body){
+    /**
+     * Initializes an instance of Comment.
+     *
+     * @param commenter The user that posted this comment.
+     * @param body The text content of the comment.
+     * @param numCommentsCreated The number of comments created in the program so far.
+     */
+    public Comment(User commenter, String body, int numCommentsCreated){
         super.user = commenter;
         super.body = body;
         super.replies = new ArrayList<>();
-        creationDate = new Date();
-
+        super.creationDate = new Date();
+        Comment.numComments = numCommentsCreated;
+        Comment.numComments ++;
+        this.id = Comment.numComments;
     }
+
+    /**
+     * Returns the body (or text content) of this instance of Comment.
+     */
     public String getBody(){
         return super.body;
     }
 
+    /**
+     * Returns the User that created this Comment.
+     */
     public User getUser(){
         return super.user;
     }
 
-
-
-
-
+    /**
+     * Return if Comment is equal to o
+     *
+     * @param o The Object to be compared to
+     * @return boolean representing whether Comment is equal to o
+     * */
+    @Override
+    public boolean equals (Object o) {
+        if (o == null) {
+            return false;
+        } else if (!(o instanceof Comment)) {
+            return false;
+        }  else {
+            return (this.id == ((Comment) o).id);
+        }
+    }
 }
--- src/main/java/entities/Postable.java
+++ src/main/java/entities/Postable.java
@@ -1,15 +1,19 @@
 package entities;
 
 import java.util.List;
+import java.util.Date;
 
 abstract class Postable{
+    // The access modifiers have been omitted so that the default settings would be applied.
+    // If we make these instance attributes private they become inaccessible to the child classes.
     User user;
     String body;
-
     List<Postable> replies;
+    Date creationDate;
 
     public void addComment(Comment comment){
-        this.replies.add(comment);
+        this.replies.append(comment);
     }
+
 }
 
