c31fd83c0d1cd65692873172d2eb31f032013039
BerryRahul <113147217+BerryRahul@users.noreply.github.com>
2022-11-19 19:14:49 -0500

    Revised my LogOut view to implement PropertyChangeListener Instead

--- src/main/java/LogOut/ui/LogOutView.java
+++ src/main/java/LogOut/ui/LogOutView.java
@@ -1,15 +1,15 @@
 package LogOut.ui;
 
 import LogOut.interface_adapters.LogOutController;
-import LogOut.use_case.LogOutInputBoundary;
-import login.interface_adapters.LogInController;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
-public class LogOutView extends JPanel implements ActionListener {
+public class LogOutView extends JPanel implements PropertyChangeListener {
 
     private final LogOutController controller;
 
@@ -18,20 +18,14 @@ public class LogOutView extends JPanel implements ActionListener {
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-        String logOut = "";
-
-        if (e.getActionCommand().equals("Log Out")){
-            logOut = "Log Out";
-        }
-
-        LogOutController logOutRequest = new LogOutController(logOut);
-        controller.LogOutInitializer(logOutRequest);
+    public void propertyChange(PropertyChangeEvent evt) {
+        if (evt.getPropertyName().equals("Log Out")){
+            String logOut = "Log Out";
+            LogOutController logOutController = new LogOutController(logOut);
+            controller.LogOutInitializer(logOutController);
 
 
-        // I'm not sure if this is the correct way to set the main page view to the Main login/signup view
-        CardLayout cardLayout = (CardLayout) this.getLayout();
-        if (e.getSource().equals("logOutButton")){
+            CardLayout cardLayout = (CardLayout) this.getLayout();
             cardLayout.show(this, "Main View");
         }
     }
