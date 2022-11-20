8585594dd7c121dab529d3c9159061596919d424
BerryRahul <113147217+BerryRahul@users.noreply.github.com>
2022-11-19 19:17:38 -0500

    Revised my LogOut view to implement PropertyChangeListener Instead

--- src/main/java/LogOut/ui/LogOutView.java
+++ src/main/java/LogOut/ui/LogOutView.java
@@ -4,8 +4,6 @@ import LogOut.interface_adapters.LogOutController;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
