42db521d217433c16e2d08d959bc5c8bc3a8a142
Richard Shi <richard.540102@gmail.com>
2022-11-20 01:32:09 -0500

    Create the view post use case.

---
+++ src/main/java/view_post/interface_adapters/ViewPostController.java
@@ -0,0 +1,26 @@
+package view_post.interface_adapters;
+
+import view_post.use_case.ViewPostInputBoundary;
+import view_post.use_case.ViewPostRequestModel;
+
+public class ViewPostController {
+    private final ViewPostInputBoundary interactor;
+
+    /**
+     * Initialize a ViewPostController object.
+     * @param interactor    A ViewPostInputBoundary object that executes the use case.
+     */
+    public ViewPostController(ViewPostInputBoundary interactor) {
+        this.interactor = interactor;
+    }
+
+    /**
+     * Execute this use case.
+     * @param postID The id of the post the user wishes to view.
+     */
+    public void viewPost(int postID) {
+        ViewPostRequestModel inputData = new ViewPostRequestModel(postID);
+
+        interactor.displayPost(inputData);
+    }
+}
