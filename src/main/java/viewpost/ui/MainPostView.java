12e0103ece47834fff43dfd5d32a5fc2a182f0a3
izumilocal <izumi.ando@mail.utoronto.ca>
2022-11-20 21:04:46 -0500

    - updated MainPostView

--- src/main/java/viewpost/ui/MainPostView.java
+++ src/main/java/viewpost/ui/MainPostView.java
@@ -4,11 +4,18 @@ import javax.swing.*;
 
 public class MainPostView {
     private JFrame mainpostview;
+    private ViewPostView viewPost;
+    private PostListView postList;
 
     public MainPostView(){
         this.mainpostview = new JFrame();
         this.mainpostview.setSize(900,800);
+        // adding view post view
+        this.viewPost = new ViewPostView();
+        this.mainpostview.add(this.viewPost.getViewPostPanel());
+        this.viewPost.getViewPostPanel().setBounds(300,120,600,680);
+        // adding post list view
+        this.postList = new PostListView();
     }
 
-    public
 }
