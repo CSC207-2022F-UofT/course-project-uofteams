7513391afe909d7987e223bf00faccd700b3fa6b
BerryRahul <113147217+BerryRahul@users.noreply.github.com>
2022-12-07 15:28:20 -0500

    Current LogOutView Implementation.

--- src/main/java/log_out/ui/LogOutView.java
+++ src/main/java/log_out/ui/LogOutView.java
@@ -16,6 +16,10 @@ public class LogOutView extends JPanel implements PropertyChangeListener, Action
 
     public JButton logOutButton = new JButton("logOutButton");
 
+    private JPanel MasterLandingView;
+
+
+
     public LogOutView(LogOutController controller){
         this.controller = controller;
 
@@ -25,15 +29,8 @@ public class LogOutView extends JPanel implements PropertyChangeListener, Action
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         if (evt.getPropertyName().equals("Log Out")){
-            String logOut = "log out";
-
-            //CardLayout cardLayout = (CardLayout) this.masterView.getLayout;
-            //cardLayout.show(this, "Main View");
-
-            // will need the implementation of the main view which contains the 'Log Out' button
-            // and then switch that view with the Main LogIn/SignUp view
-            // also will need to add a param in my constructor that takes a masterView, so I can call the
-            // get layout function in order to show it.
+            CardLayout cardLayout = new CardLayout();
+            cardLayout.show(MasterLandingView, "");
         }
     }
 
