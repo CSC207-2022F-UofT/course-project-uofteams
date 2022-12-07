65e1df10b5b3c469ce040a2784e5bafae1cba4e1
BerryRahul <113147217+BerryRahul@users.noreply.github.com>
2022-12-07 14:38:16 -0500

    Forgot to commit LogOutTest

---
+++ src/test/java/log_out/LogOutTest.java
@@ -0,0 +1,60 @@
+package log_out;
+
+import entities.CurrentUser;
+import entities.User;
+import log_in.use_case.LogInOutputBoundary;
+import log_in.use_case.LogInResponseModel;
+import log_out.interface_adapters.LogOutController;
+import log_out.interface_adapters.LogOutControllerData;
+import log_out.use_case.LogOutInputBoundary;
+import log_out.use_case.LogOutInteractor;
+import log_out.use_case.LogOutOutputBoundary;
+import log_out.use_case.LogOutResponseModel;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class LogOutTest {
+
+    LogOutInputBoundary interactor;
+
+    LogOutOutputBoundary presenter;
+
+    LogOutController controller;
+
+
+    public final List<User> users = new ArrayList<>();
+
+    // method for testing purposes will update when csv is up
+    public void addUser(User user){
+        users.add(user);
+    }
+
+
+    @Test
+    public void logOutSuccess() {
+        presenter = new LogOutOutputBoundary() {
+            @Override
+            public void present(LogOutResponseModel responseModel) {
+                boolean creation = responseModel.isSuccess();
+
+                assertTrue(creation);
+                assertNull(CurrentUser.getCurrentUser());
+            }
+        };
+
+        interactor = new LogOutInteractor(presenter);
+        User user = new User(false, 0, "a", "b");
+        CurrentUser.setCurrentUser(user);
+        this.addUser(user);
+
+        controller = new LogOutController(interactor);
+        LogOutControllerData test = new LogOutControllerData("Log Out");
+
+        controller.logOutInitializer(test);
+    }
+}
